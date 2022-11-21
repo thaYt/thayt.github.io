655ea5e42c12cde74ffecd06d79b0852d58fdca6
core <thayt@jonahm.dev>
2022-11-21 16:48:06 -0500

    Updates

--- _app/immutable/chunks/1-3036c9d4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-77e04acd.js";export{t as component};
--- _app/immutable/chunks/2-98145e2f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-7001817f.js";export{t as component};
